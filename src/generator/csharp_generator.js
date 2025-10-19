9df5d0290d4289afc2af3a7d490accacb3e93432
Guliycagen <Guliycagen@gmail.com>
2025-10-19 15:01:44 +0700

    v1.0

--- src/generator/csharp_generator.js
+++ src/generator/csharp_generator.js
@@ -4,7 +4,9 @@
 import { csharpGenerator } from './generator_instance.js';
 
 // 2. Подключаем все остальные файлы, чтобы они "зарегистрировали" свои блоки.
-// Теперь они все будут импортировать csharpGenerator из одного и того же места.
+// Это называется "импорт для побочных эффектов" (import for side effects).
+// Мы не используем ничего из этих файлов напрямую здесь, но сам факт их импорта
+// заставляет их код выполниться.
 import './standard_csharp_generator.js';
 import './se_csharp_generator.js';
 import './se_csharp_base_generator.js';
